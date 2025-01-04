e50e837e2d7fe860a08b9145b300f5609d62fab1
samm <sm9609@rit.edu>
2025-01-03 23:03:10 -0500

    - Main - WordleConfig

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        WordleConfig game = new WordleConfig();
+    public static void main(String[] args) throws FileNotFoundException {
+        try {
+            File File = new File("data/5LetterWords");
+            System.out.println("File Found");
+
+            BufferedReader reader = new BufferedReader(new FileReader(File));
+            String[] words = (String[]) reader.lines().toArray();
+            String secretWord = words[(int)(Math.random()+words.length)];
+            WordleConfig game = new WordleConfig(secretWord,words);
+        }catch (IOException e){
+            throw e;
+        }
     }
 }
--- src/WordleConfig.java
+++ src/WordleConfig.java
@@ -1,45 +1,97 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.function.Predicate;
+
 public class WordleConfig {
     public enum Guess {
         GREEN,
         YELLOW,
         RED
     }
+    /*fields*/
+
+    private final String secretWord;
+    private final String[] words;
 
     static int MAX_MOVES = 6;
-    private Guess[][] grid;
-    private String secretWord;
-    private int wordLength;
+    private String[][] grid;
+    private final int secretWordLength;
     private int attempts;
 
-    public WordleConfig(String secretWord){
+    private String currentGuess;
+    private String[] correctGuesses;
+    private HashMap<Character,Guess> guessedLetters;
+    private ArrayList<String> correctLetters;
+    private ArrayList<String> wrongLetters;
+
+    /*Constructor*/
+    public WordleConfig(String secretWord,String[] words){
         this.secretWord = secretWord;
-        this.grid = new Guess[MAX_MOVES][secretWord.length()];
-        this.wordLength = secretWord.length();
+        this.words = words;
+        this.grid = new String[MAX_MOVES][secretWord.length()];
+        this.secretWordLength = secretWord.length();
+        this.guessedLetters = new HashMap<Character,Guess>();
+        this.correctGuesses = new String[secretWordLength];
+        this.correctLetters = new ArrayList<String>();
+        this.wrongLetters = new ArrayList<String>();
+
         this.attempts = 0;
     }
 
-    private Guess tryLettter(char l, int index){
+    /* Player Function */
+    private String tryLetter(char l, int index){
         if(secretWord.charAt(index) == l){
-            return Guess.GREEN;
+            guessedLetters.put(secretWord.charAt(index),Guess.GREEN);
+            correctLetters.add(String.valueOf(l));
         }
         else if(secretWord.contains(String.valueOf(l))){
-            return  Guess.YELLOW;
+            guessedLetters.put(secretWord.charAt(index),Guess.YELLOW);
+            correctLetters.add(String.valueOf(l));
         }
         else{
-            return Guess.RED;
+            guessedLetters.put(secretWord.charAt(index),Guess.RED);
+            wrongLetters.add(String.valueOf(l));
         }
+        return String.valueOf(l);
     }
 
     public Boolean guessWord(String word){
-        if(word.length() != wordLength){
+        if(word.length() != secretWordLength){
             return false;
         }else {
             String[] guess = word.split("");
-            for (int i = 0; i < wordLength; i++) {
-               grid[attempts][i] = tryLettter(guess[i].charAt(0),i);
+            for (int i = 0; i < secretWordLength; i++) {
+               grid[attempts][i] = tryLetter(guess[i].charAt(0),i);
+            }
+            attempts++;
+            return true;
+        }
+    }
 
+    /*Solver Functions*/
+    public boolean isSolution(){
+        return (Objects.equals(currentGuess, secretWord));
+    }
+
+    public void getPossibleWords(){
+
+        Predicate<String> hasPossibleLetters = str -> {
+            if(str.length() != secretWordLength){
+                return false;
+            }
+            for(int i = 0; i < str.length(); i++){
+                if(){
+                    return false;
+                }
             }
             return true;
+        };
+
+        if(!Arrays.stream(correctGuesses).allMatch(null)){
+            Arrays.stream(words)
+                    .filter(hasPossibleLetters);
         }
     }
 
